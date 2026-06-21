8f2dbb8fa68edb99120657efe191e16d59e57fee
pappigo <yjs981020@gmail.com>
2026-06-21 12:57:15 +0900

    에러조건 변경

--- src/main/java/com/example/board/user/UserProfileService.java
+++ src/main/java/com/example/board/user/UserProfileService.java
@@ -1,6 +1,8 @@
 package com.example.board.user;
 
 import com.example.board.global.entity.UserProfile;
+import com.example.board.global.exception.ErrorCode;
+import com.example.board.global.exception.UnauthorizedException;
 import com.example.board.user.dto.UserProfileResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,7 +14,7 @@ public class UserProfileService {
 
     public UserProfileResponse me(Long loginUserId) {
         UserProfile user = userProfileRepository.findById(loginUserId)
-                .orElseThrow(() -> new RuntimeException("프로필을 찾을 수 없습니다."));
+                .orElseThrow(() -> new UnauthorizedException(ErrorCode.LOGIN_REQUIRED));
 
         return UserProfile.toDTO(user);
     }
